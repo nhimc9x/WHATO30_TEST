0fb64c453218281bc1d9ea67065922319e25d351
nhimc9x <nhicomputer77@gmail.com>
2024-08-21 11:49:09 +0700

    Base

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Test } from '~/components/Test/Test'
+
+function App() {
+  return (
+    <>
+      <Test />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Test/Test.tsx
@@ -0,0 +1,8 @@
+
+export const Test = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
