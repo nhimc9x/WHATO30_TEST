60379fc9a9977610d39b73232067323011de4f18
nhimc9x <nhicomputer77@gmail.com>
2024-08-21 15:55:31 +0700

    Configure base colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Montserrat': ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'ct-orange-color': '#f96400',
+        'ct-yellow-color': '#f9c200',
+        'ct-blue-color': '#0565bb',
+        'ct-black-color': {
+          'bold': '#111111',
+          'normal': '#222222',
+          'light': '#333333',
+        },
+        'ct-border-color': '#020001',
+      },
+    },
   },
   plugins: [],
 }
